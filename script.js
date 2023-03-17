7b28809ad16635d4ec1f42074d31dea295744ebb
Gustavo Muller <gusrmuller@gmail.com>
2023-03-17 20:53:46 -0300

    implement criteria for strong password

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let parameters = {
+    count: false,
+    letters: false,
+    numbers: false,
+    special: false
+}
+let strengthBar = document.getElementById('strength-bar');
+let msg = document.getElementById('msg');
+
+function strengthChecker() {
+    let password = document.getElementById('password').value;
+
+    parameters.letters = (/[A-Za-z]+/.test(password)) ? true : false;
+    parameters.numbers = (/[0-9]+/.test(password)) ? true : false;
+    parameters.special = (/[!\"$%&/()=?@`\\.\';:+=^*_-]+/.test(password)) ? true : false;
+    parameters.count = (password.length > 7) ? true : false;
+
+    console.log(Object.values(parameters));
+}
+
 function toggle() {
     let password = document.getElementById('password');
     let eye = document.getElementById('toggle');
